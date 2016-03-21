184a06feebc039fb268ff81401cf870dc3658b3d
ankitjaggi <ankit.jaggi@live.in>
2016-03-21 11:26:05 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var chalk = require('chalk');
+
+module.exports = {
+	log: function(text) {
+		var final_text = formatDateTime() + String(text);
+		console.log(final_text);
+	},
+
+    error: function(text) {
+        var final_text = formatDateTime() + String(text);
+        console.log(chalk.red(final_text));
+    }	
+};
+
+function formatDateTime() {
+	var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return "[" + year + "-" + month + "-" + day + " " + hour + ":" + min + ":" + sec + "] : ";
+}
